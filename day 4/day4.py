af56ae65c3016a0c46f8bb18a9ff4d28a0e5f43e
opitt <23746293+opitt@users.noreply.github.com>
2022-12-03 08:24:02 +0100

    Prepare for day 4

---
+++ day 4/day4.py
@@ -0,0 +1,34 @@
+# https://adventofcode.com/2022/day/4
+import os
+from unittest import result
+
+def solve1(lines):
+    result=0
+    print(
+        f"... is {result}"
+    )
+    return result
+
+def solve2(lines):
+    result=0
+    print(
+        f"... is {result}"
+    )    
+    return result
+    
+def main(test):
+
+    # READ INPUT FILE
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(script_path, "test.txt" if test else "input.txt"), encoding="utf-8") as input:
+        lines = input.readlines()
+    lines = list(map(str.strip, lines))
+    
+    # PART 1
+    solve1(lines) # ==
+
+    # PART 2
+    solve2(lines) # == 
+
+main(test=True)
+#main(test=False)
