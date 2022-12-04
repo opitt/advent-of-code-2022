06d41d3c088077d8d9112bb5556908369ff5d633
opitt <23746293+opitt@users.noreply.github.com>
2022-12-04 07:06:10 +0100

    make more readable

--- day 4/day4.py
+++ day 4/day4.py
@@ -2,32 +2,41 @@
 import os
 
 def solve1(lines):
+    """Count pairs with fully overlapping sectors
+
+    Args:
+        lines (list): a list of elf-pair-sectors e.g.: 2-9,6-8
+
+    Returns:
+        int: number of elf pairs with at least one fully overlapping sectors
+    """
     result=0
     for line in lines:
-        #2-4,6-8
-        elf1, elf2 = [list(map(int,elf.split("-"))) for elf in line.split(",")]
-        print(elf1,elf2)
-        s1 = elf1[0] <= elf2[0] and elf1[1] >= elf2[1] 
-        s2 = elf2[0] <= elf1[0] and elf2[1] >= elf1[1] 
-        result += s1 or s2
+        elf1_sector, elf2_sector = [list(map(int,sector.split("-"))) for sector in line.split(",")]
+        isOverlapping=lambda s1,s2: s1[0] <= s2[0] and s1[1] >= s2[1] 
+        result += isOverlapping(elf1_sector,elf2_sector) or isOverlapping(elf2_sector,elf1_sector)
 
     print(
-        f"... is {result}"
+        f"Part 1 ... is {result}"
     )
     return result
 
 def solve2(lines):
+    """Count pairs with partially overlapping sectors
+
+    Args:
+        lines (list): a list of elf-pair-sectors e.g.: 2-7,6-8
+
+    Returns:
+        int: number of elf pairs with at least partially overlapping sectors
+    """
     result=0
     for line in lines:
-        #2-4,6-8
-        elf1, elf2 = [list(map(int,elf.split("-"))) for elf in line.split(",")]
-        #print(elf1,elf2)
-        s1 = elf1[1] < elf2[0] or elf1[0] > elf2[1]
-        #s2 = elf2[0] > elf1[1] or elf2[1] < elf1[0]
-        result += s1 # s2
+        elf1_sector, elf2_sector = [list(map(int,sector.split("-"))) for sector in line.split(",")]
+        result += elf1_sector[1] < elf2_sector[0] or elf1_sector[0] > elf2_sector[1]
 
     print(
-        f"... is {len(lines)-result}"
+        f"Part 2 ... is {len(lines)-result}"
     )    
     return result
     
@@ -39,10 +48,10 @@ def main(test):
         lines = input.read().strip().split("\n")
     
     # PART 1
-    solve1(lines) # ==
+    solve1(lines) # == 542
 
     # PART 2
-    solve2(lines) # == 
+    solve2(lines) # == 900
 
 #main(test=True)
 main(test=False)
