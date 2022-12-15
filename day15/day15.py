261fa970f0009ab620bf3481fa42bc3b98124134
opitt <23746293+opitt@users.noreply.github.com>
2022-12-16 00:06:16 +0100

    Solution part 1

--- day15/day15.py
+++ day15/day15.py
@@ -2,10 +2,29 @@
 from rich import print
 import os
 from time import time
+import re
 
-def solve():
-    res=0
-    print(f"Solution ... {res}")
+
+def solve(lines, testline):
+    coverage = []
+    beacons = set()
+    for line in lines:
+        # Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        sx, sy, bx, by = list(map(int, re.findall("[-]?\d+", line)))
+        s_range = abs(sx-bx)+abs(sy-by)
+        if sy-s_range <= testline <= sy+s_range:
+            # the sensor covers the interesting row
+            # get the min x and the max x, which depends on the y of the sensor and the sensor range
+            s_coverage_testline = (sx-(s_range-(abs(testline-sy))),
+                                   sx+(s_range-(abs(testline-sy))))
+            coverage.append(s_coverage_testline)
+            if by == testline: # there might be a beacon in the row already (test case)
+                beacons.add(bx)
+        pass
+    #coverage.sort()
+    max_x = max(coverage, key=lambda x: x[1])[1]
+    min_x = min(coverage, key=lambda x: x[0])[0]
+    print("Solution 1 is", max_x - min_x + 1 - len(beacons))
 
 
 def main(test):
@@ -18,17 +37,18 @@ def main(test):
     ) as input:
         lines = input.read().rstrip().split("\n")
 
+    if test:
+        testline = 10
+    else:
+        testline = 2000000
 
     # PART 1
     start = time()
-    solve(lines)
+    solve(lines, testline)
     print(time() - start, " seconds")
 
     # PART 2
-    start = time()
-    #solve2(lines)
-    print(time() - start, " seconds")
 
 
-main(test=True)  # 
-#main(test=False)  # 
+main(test=True)  # 26, 4725496
+main(test=False)  #
