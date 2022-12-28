d6a1beb576433981c4eac88d6a89c665555d9699
opitt <23746293+opitt@users.noreply.github.com>
2022-12-28 10:05:32 +0100

    Part 2 note working yet

--- day 24/day24.py
+++ day 24/day24.py
@@ -6,43 +6,143 @@ from time import time, sleep
 from rich import print
 from rich.table import Table
 from rich.live import Live
+import re
 
 
-def solve(left, right, up, down, startx, destx):
+class Blizzard():
 
-    def mergeblizz(left, right, up, down):
-        blizz = []
-        for y in range(len(left)):
-            blizz.append([])
-            for x in range(len(left[0])):
-                l, r, u, d = left[y][x], right[y][x], up[y][x], down[y][x]
+    def __init__(self, lines, history=False):
+        self.left = []
+        self.right = []
+        self.up = []
+        self.down = []
+        self.blizzmap = []
+        self.EMPTY = "."
+        self.minutes = 0
+        #
+        self.history = history
+        self.blizzmap_history = []
+        #self.maxx = 0
+        #self.maxy = 0
+        self.__parse_blizzards(lines)
+        self.__merge()
+
+    def __parse_blizzards(self, lines):
+        self.left = []
+        self.right = []
+        self.up = []
+        self.down = []
+        for line in lines[1:-1]:
+            self.left.append([])
+            self.right.append([])
+            self.up.append([])
+            self.down.append([])
+            y = -1  # the last line added
+            o = self.EMPTY
+            for b in line[1:-1]:
+                if b == "<":
+                    self.left[y].append(b)
+                    self.right[y].append(o)
+                    self.up[y].append(o)
+                    self.down[y].append(o)
+                elif b == ">":
+                    self.left[y].append(o)
+                    self.right[y].append(b)
+                    self.up[y].append(o)
+                    self.down[y].append(o)
+                elif b == "^":
+                    self.left[y].append(o)
+                    self.right[y].append(o)
+                    self.up[y].append(b)
+                    self.down[y].append(o)
+                elif b == "v":
+                    self.left[y].append(o)
+                    self.right[y].append(o)
+                    self.up[y].append(o)
+                    self.down[y].append(b)
+                else:
+                    self.left[y].append(b)
+                    self.right[y].append(b)
+                    self.up[y].append(b)
+                    self.down[y].append(b)
+        #
+        self.maxx = len(self.left[0])-1
+        self.maxy = len(self.left)-1
+        self.s = (0, lines[0][1:-1].index("."))
+        self.d = (len(lines)-3, lines[-1][1:-1].index("."))
+
+    def __merge(self):
+        if self.history and len(self.blizzmap):
+            self.blizzmap_history.append(
+                "\n".join("".join(line) for line in self.blizzmap))
+        self.blizzmap = []
+        for y in range(len(self.left)):
+            self.blizzmap.append([])
+            for x in range(len(self.left[0])):
+                l, r, u, d = self.left[y][x], self.right[y][x], self.up[y][x], self.down[y][x]
+                samefield = set([l, r, u, d]) - set(self.EMPTY)
+                if len(samefield) == 0:
+                    v = self.EMPTY
+                elif len(samefield) == 1:
+                    v = list(samefield)[0]
+                else:
+                    v = str(len(samefield))
                 # overlay the blizzard, just for visuality
-                v = u
-                v = v if d == "." else d
-                v = v if l == "." else l
-                v = v if r == "." else r
-                blizz[y].append(v)
-        return blizz
+                #v = u
+                #v = v if d == self.EMPTY else d
+                #v = v if l == self.EMPTY else l
+                #v = v if r == self.EMPTY else r
 
-    def moveblizz(left, right, up, down):
-        down = [down[-1], *down[:-1]]
-        up = [*up[1:], up[0]]
-        left = [[*line[1:], line[0]] for line in left]
-        right = [[line[-1], *line[:-1]] for line in right]
-        return left, right, up, down
+                self.blizzmap[y].append(v)
 
-    def visualise(blizzmap, q, minutes) -> Table:
+    def __move(self):
+        self.down = [self.down[-1], *self.down[:-1]]
+        self.up = [*self.up[1:], self.up[0]]
+        self.left = [[*line[1:], line[0]] for line in self.left]
+        self.right = [[line[-1], *line[:-1]] for line in self.right]
+
+    def print(self, visiting=[]):
+        print(f"*** Minute {self.minutes}")
+        if len(visiting
+               ):
+            temp = deepcopy(self.blizzmap)
+            for y, x in visiting:
+                temp[y][x] = "E"
+            print("\n".join("".join(line) for line in temp))
+        else:
+            print("\n".join("".join(line) for line in self.blizzmap))
+        print(f"")
+
+    def map_nextmin(self):
+        self.minutes += 1
+        self.__move()
+        self.__merge()
+
+    def isempty(self, y, x):
+        return self.blizzmap[y][x] == self.EMPTY
+
+    def reachable_fields(self, y, x):
+        fields = [(max(0, y-1), x), (min(self.maxy, y+1), x),  # y+/- down/up
+                  (y, max(0, x-1)), (y, min(self.maxx, x+1)),  # x+/- right/left
+                  (y, x)]  # wait
+        return set(fields)
+
+
+def solve(blizz, first, last):
+
+    def visualise(blizz, visiting) -> Table:
         table = Table()
-        table.add_column(f"Map after {minutes} minutes", justify="center")
-        bm = deepcopy(blizzmap)
-        for y, x in q:
+        table.add_column(
+            f"Map after {blizz.minutes} minutes", justify="center")
+        bm = deepcopy(blizz.blizzmap)
+        for y, x in visiting:
             bm[y][x] = "@"
         for line in bm:
             l = "".join(line)
             l = l.replace(
                 "@", "[bright_white on red]@[/bright_white on red]")
             l = l.replace(
-                ".", "[bright_white on white] [/bright_white on white]")
+                ".", "[white on white].[/white on white]")
             l = l.replace(
                 ">", "[bright_white on deep_sky_blue2]>[/bright_white on deep_sky_blue2]")
             l = l.replace(
@@ -54,95 +154,41 @@ def solve(left, right, up, down, startx, destx):
             table.add_row(f"{l}")
         return table
 
-    # merge the blizzards to the initial map
-    blizzmap = mergeblizz(left, right, up, down)
-    maxy = len(blizzmap)-1
-    maxx = len(blizzmap[0])-1
-    minutes = 0
-
-    # wait until elf can move into the map from the start pos (border)
+    visiting = deque()
+    # no field visiting yet. Elf is sitting on the edge, waiting that they can can step to the FIRST field on map
     while True:
-        minutes += 1
-        print(minutes)
-        left, right, up, down = moveblizz(left, right, up, down)
-        blizzmap = mergeblizz(left, right, up, down)
-        if blizzmap[0][startx] == ".":
+        blizz.map_nextmin()
+        blizz.print(visiting)
+        if blizz.isempty(*first):  # can elf visit the first in that minute
+            # step into the field
+            visiting.append(first)
             break
 
-    q = deque()
-    q.append((0, startx))
+    while visiting and last not in visiting:
+        # next round
+        blizz.map_nextmin()
+        # try to move the elfs
+        elfs = len(visiting)
+        for _ in range(elfs):
+            # check all elfs currently visiting fields
+            #   if they can move during this new minutet. If they can't move anymore, they die ... :(
+            fields = blizz.reachable_fields(*visiting.popleft())
+            visit_nextmin = [(y, x) for y, x in fields if blizz.isempty(
+                y, x) and (y, x) not in visiting]
+            visiting.extend(visit_nextmin)
+        blizz.print(visiting)
+    # the next minute is used to step to the target
+    if last in visiting:
+        # the elf steps in the next minute to the desition field
+        blizz.map_nextmin()
+    else:
+        raise ValueError(f"Did not reach the destination {last}")
 
-    with Live(visualise(blizzmap, q, minutes), refresh_per_second=4) as live:
-        while q:
-            if VIS:
-                sleep(0.1)
-                live.update(visualise(blizzmap, q, minutes))
-
-            for _ in range(len(q)):
-                # check all elfs, where they can move. If they can't move anymore, they die ... :(
-                elfy, elfx = q.popleft()
-                moves = set([(max(0, elfy-1), elfx), (min(maxy, elfy+1), elfx),  # y+/- down/up
-                            (elfy, max(0, elfx-1)), (elfy,
-                                                     min(maxx, elfx+1)),  # x+/- right/left
-                            (elfy, elfx)])  # wait
-
-                actions = [(y, x) for y, x in moves if blizzmap[y]
-                           [x] == "." and (y, x) not in q]
-                if (maxy, destx) in actions:
-                    q.clear()
-                    break
-                else:
-                    q.extend(actions)
-            minutes += 1
-            left, right, up, down = moveblizz(left, right, up, down)
-            blizzmap = mergeblizz(left, right, up, down)
-        live.update(visualise(blizzmap, [(maxy, destx)], minutes))
-    print("Map shows the position of the elf ABOVE the exit.")
-    print("Destination reached after (mins): ", minutes)
-
-def parse_blizzards(lines):
-    left = []
-    right = []
-    up = []
-    down = []
-
-    for line in lines[1:-1]:
-        left.append([])
-        right.append([])
-        up.append([])
-        down.append([])
-        y = -1
-        for b in line[1:-1]:
-            o = "."
-            if b == "<":
-                left[y].append(b)
-                right[y].append(o)
-                up[y].append(o)
-                down[y].append(o)
-            elif b == ">":
-                left[y].append(o)
-                right[y].append(b)
-                up[y].append(o)
-                down[y].append(o)
-            elif b == "^":
-                left[y].append(o)
-                right[y].append(o)
-                up[y].append(b)
-                down[y].append(o)
-            elif b == "v":
-                left[y].append(o)
-                right[y].append(o)
-                up[y].append(o)
-                down[y].append(b)
-            else:
-                left[y].append(b)
-                right[y].append(b)
-                up[y].append(b)
-                down[y].append(b)
-    return left, right, up, down
-
-
-VIS = False
+# Minute 17
+# 2.v.<>
+# <.<..<
+# .^>^22
+# .2..2E
 
 
 def main(test):
@@ -155,15 +201,29 @@ def main(test):
     with open(os.path.join(script_path, filename), encoding="utf-8") as input:
         lines = input.read().rstrip().split("\n")
 
-    left, right, up, down = parse_blizzards((lines))
-    startx = lines[0][1:-1].index(".")
-    destx = lines[-1][1:-1].index(".")
+    blizz = Blizzard(lines, history=True)
+    # is the last line of the maze, but actually it is the next line (border)
 
-    # PART 1 and PART 2
+    # PART 1
     start = time()
-    solve(left, right, up, down, startx, destx)
+
+    blizz.print()
+    solve(blizz, blizz.s, blizz.d)
+    print("Solution 1 to START->DEST:", blizz.minutes)
+    blizz.print()
+
+    # PART 2
+    print("Solve start->DEST->START ...", blizz.minutes)
+    solve(blizz, blizz.d, blizz.s)
+    print("Solution 2-1:")
+    blizz.print()
+    print("Solve start->dest->START->DEST ...", blizz.minutes)
+    solve(blizz, blizz.s, blizz.d)
+    blizz.print()
+    print("Solution 2-2::")
+
     print(f"{time() - start:5f} seconds")
 
 
-# main(test=True)  #
-main(test=False)  #
+main(test=True)  #
+# main(test=False)  #
